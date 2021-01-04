e45d8e471408dc18ba2394fcfbe81d2a5845c4b6
Duhyeuk Chang <54429808+justin95214@users.noreply.github.com>
2021-01-04 18:01:35 +0900

    Create main.c

---
+++ Stack/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "IntStack.h"
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable: 4996)
+
+
+
+int main(){
+
+	IntStack s;
+	if (Initialize(&s, 64) == -1) {
+		puts("스택 생성 실패");
+		return 1;
+	}
+
+	while (1)
+	{
+		int menu, x;
+		printf("현재 데이터두 : %d / %d\n", Size(&s), Capcity(&s));
+		printf("(1)push (2)pop (3)peek (4)print (5)exit\n");
+		scanf("%d", &menu);
+
+		if (menu == 0)break;
+		switch (menu)
+		{
+		case 1:
+			printf("데이터 :");
+			scanf("%d", &x);
+			if (Push(&s, x) == -1)
+				puts("push failed!!");
+			break;
+
+		case 2:
+			if (Pop(&s, x) == -1)
+				puts("pop failed!!");
+			else
+				printf("pop data = %d \n", x);
+			break;
+
+		case 3:
+
+			if (Peek(&s, x) == -1)
+				puts("peek failed!!");
+			else
+				printf("peek data = %d \n", x);
+			break;
+
+
+		case 4:
+
+			break;
+
+		}
+
+
+	}
+
+	return 0;
+}
